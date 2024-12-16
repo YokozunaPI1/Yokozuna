14c0e402b60692ac6b1e91d1060a35e8e2e5cbca
forsen2 <olihb25@hotmail.com>
2024-12-16 16:35:56 -0300

    motor etc

---
+++ motor1/motor1.ino
@@ -0,0 +1,73 @@
+int in1=2;
+int in2=4;
+int enA=3;
+
+int in3=5;
+int in4=7;
+int enB=6;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(enA, OUTPUT);
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  pinMode(enB, OUTPUT);
+  pinMode(in3, OUTPUT);
+  pinMode(in4, OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
+  digitalWrite(enA, 100);
+  digitalWrite(enB, 100);
+  delay(1500);
+
+
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, LOW);
+  digitalWrite(enA, 100);
+  digitalWrite(enB, 100);
+  delay(1500);
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
+  digitalWrite(enA, 100);
+  digitalWrite(enB, 100);
+  delay(1500);
+
+
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, LOW);
+  digitalWrite(enA, 100);
+  digitalWrite(enB, 100);
+  delay(1500);
+
+
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, HIGH);
+  digitalWrite(enA, 100);
+  digitalWrite(enB, 100);
+  delay(1500);;
+
+
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, HIGH);
+  digitalWrite(enA, 100);
+  digitalWrite(enB, 100);
+  delay(1500);
+}
